5b2f6db5c6f65c98804068c9d6a8087572281da1
Evin Dunn <evindunn14@gmail.com>
2021-03-08 10:44:01 -0700

    Updates api plugin generator

--- tools/generators/api-plugin/index.ts
+++ tools/generators/api-plugin/index.ts
@@ -47,6 +47,35 @@ function updateTsConfig(host: Tree, schema: ApiPluginSchema): Rule {
     };
 }
 
+function updatePackageJson(host: Tree, schema: ApiPluginSchema): Rule {
+    return (host: Tree, context: SchematicContext) => {
+        const projectConf = getProjectConfig(host, getProjectName(schema.name));
+
+        return updateJsonInTree(
+            `${projectConf.root}/package.json`,
+            (json) => {
+                return Object.assign(
+                    {},
+                    json,
+                    {
+                        "license": "GPL-3.0-or-later",
+                        "peerDependencies": {
+                            "@nestjs/common": "^7.6.13",
+                            "@nestjs/config": "^0.6.3",
+                            "reflect-metadata": "^0.1.13",
+                            "rxjs": "^6.6.6",
+                            "typeorm": "~0.2.31"
+                        },
+                        "devDependencies": {
+                            "typescript": "~4.1.0"
+                        }
+                    }
+                );
+            }
+        );
+    }
+}
+
 export default function(schema: ApiPluginSchema): Rule {
     if (!schema.name.match(PACKAGE_REGEX)) {
         throw new Error('The plugin name must be in the format @<my-org>/<my-plugin>');
@@ -55,7 +84,8 @@ export default function(schema: ApiPluginSchema): Rule {
     return (host: Tree, context: SchematicContext) => {
         const chainedRules = chain([
             generateNestLibrary(schema),
-            updateTsConfig(host, schema)
+            updateTsConfig(host, schema),
+            updatePackageJson(host, schema)
         ]);
 
         return chainedRules(host, context);
