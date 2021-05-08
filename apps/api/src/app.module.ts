8ea425f62d90025a8504fe63204858988bf36f11
Evin Dunn <evindunn14@gmail.com>
2021-05-08 11:25:02 -0700

    Breaks core api AppModule dependency on DatabaseModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from '@symbiota2/api-config';
-import { DatabaseModule } from '@symbiota2/api-database';
-import { LanguageModule } from './language/language.module';
 import { UserModule, AuthModule } from '@symbiota2/api-auth';
 import { ApiPluginModule } from '@symbiota2/api-common';
 import { OccurrenceModule } from '@symbiota2/api-plugin-occurrence';
@@ -11,8 +9,6 @@ import { ApiJobsModule } from '@symbiota2/api-jobs';
 @Module({
     imports: [
         AppConfigModule,
-        DatabaseModule,
-        LanguageModule,
         UserModule,
         AuthModule,
         ApiPluginModule.configure([
