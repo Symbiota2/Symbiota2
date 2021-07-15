d83f62e8a54c61207da7da7c9f0520b82a373be7
Evin Dunn <evindunn14@gmail.com>
2021-07-14 17:49:27 -0700

    Log error if password reset fails

--- libs/api-auth/src/user/services/password-reset.processor.ts
+++ libs/api-auth/src/user/services/password-reset.processor.ts
@@ -5,10 +5,12 @@ import { ResetPasswordInputDto } from '../dto/reset-password.input.dto';
 import { EmailService } from '@symbiota2/api-email';
 import { UserService } from './user.service';
 import { randomBytes } from 'crypto';
+import { Logger } from '@nestjs/common';
 
 @Processor(QUEUE_ID_PASSWORD_RESET)
 export class PasswordResetProcessor {
     private static readonly PASSWORD_LENGTH = 6;
+    private logger = new Logger(PasswordResetProcessor.name);
 
     constructor(
         private readonly users: UserService,
@@ -17,18 +19,24 @@ export class PasswordResetProcessor {
     @Process()
     async resetPassword(job: Job<ResetPasswordInputDto>) {
         const { username } = job.data;
-        const newPassword = await PasswordResetProcessor.generatePassword();
-        const email = await this.users.resetPassword(username, newPassword);
 
-        if (email) {
-            await this.email.send(
-                'Password Reset',
-                `Your password has been reset to ${newPassword}. Please log in and change it.`,
-                email,
-            );
+        try {
+            const newPassword = await PasswordResetProcessor.generatePassword();
+            const email = await this.users.resetPassword(username, newPassword);
+
+            if (email) {
+                await this.email.send(
+                    'Password Reset',
+                    `Your password has been reset to ${ newPassword }. Please log in and change it.`,
+                    email,
+                );
+            }
+
+            // TODO: Email them if it failed?
         }
-
-        // TODO: Email them if it failed?
+        catch (e) {
+            this.logger.error(`Password reset failed for user ${username}: ${e.message}`);
+        }
     }
 
     private static async generatePassword(): Promise<string> {
