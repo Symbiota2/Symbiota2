1c51571e54ae3226e03f71058e92034d1f8257fa
Evin Dunn <evindunn14@gmail.com>
2021-11-19 07:57:24 -0700

    Makes superadmin guard extend Jwt guard

--- libs/api-auth/src/auth/guards/super-admin/super-admin.guard.ts
+++ libs/api-auth/src/auth/guards/super-admin/super-admin.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthenticatedRequest } from '../../dto/authenticated-request';
 import { TokenService } from '../../../user/services/token/token.service';
 import { AppConfigService } from '@symbiota2/api-config';
+import { JwtAuthGuard } from '../jwt-auth.guard';
 
 /**
  * Guard for checking whether the current user has the 'SuperAdmin' role in
@@ -10,12 +11,20 @@ import { AppConfigService } from '@symbiota2/api-config';
  * and https://docs.nestjs.com/guards)
  */
 @Injectable()
-export class SuperAdminGuard implements CanActivate {
-    constructor(private readonly configService: AppConfigService) { }
+export class SuperAdminGuard extends JwtAuthGuard implements CanActivate {
+    constructor(protected readonly configService: AppConfigService) {
+        super(configService);
+    }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isJwtAuthed = super.canActivate(context);
+
         if (!this.configService.isAuthEnabled()) {
             return true;
+        }
+
+        if (!isJwtAuthed) {
+            return false;
         }
 
         const http = context.switchToHttp();
