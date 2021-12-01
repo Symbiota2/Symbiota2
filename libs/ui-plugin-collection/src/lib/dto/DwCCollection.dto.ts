7671f57c16072f1cc26af3beed2db8bfc20b03d7
Darius <dtd66@nau.edu>
2021-11-30 20:51:43 -0700

    Update DwCCollection.dto.ts

--- libs/ui-plugin-collection/src/lib/dto/DwCCollection.dto.ts
+++ libs/ui-plugin-collection/src/lib/dto/DwCCollection.dto.ts
@@ -3,15 +3,6 @@ import {
     ApiPublishedCollection,
 } from '@symbiota2/data-access';
 
-export class PublishedCollection implements ApiPublishedCollection {
-    constructor(data: Record<string, unknown>) {
-        Object.assign(this, data);
-    }
-
-    collectionID: number;
-    archive: string;
-}
-
 export class CollectionArchive implements ApiCollectionArchive {
     constructor(data: Record<string, unknown>) {
         Object.assign(this, data);
