69b24b5f75d480781be3e4cd8c33233de69711a5
Evin Dunn <evindunn14@gmail.com>
2021-12-07 21:01:30 -0700

    Adds TaxonomicUnitService helpers

--- libs/api-plugin-taxonomy/src/taxonomicUnit/taxonomicUnit.service.ts
+++ libs/api-plugin-taxonomy/src/taxonomicUnit/taxonomicUnit.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { In, Repository } from 'typeorm';
+import { In, LessThan, Repository } from 'typeorm';
 import { TaxonomicUnitFindAllParams } from './dto/taxonomicUnit-find-all.input.dto';
 import { BaseService } from '@symbiota2/api-common';
 import { TaxonomicUnit } from '@symbiota2/api-database';
@@ -8,9 +8,9 @@ import { TaxonomicUnit } from '@symbiota2/api-database';
 export class TaxonomicUnitService extends BaseService<TaxonomicUnit>{
     constructor(
         @Inject(TaxonomicUnit.PROVIDER_ID)
-        private readonly myRepository: Repository<TaxonomicUnit>) {
+        private readonly taxonRanks: Repository<TaxonomicUnit>) {
 
-        super(myRepository);
+        super(taxonRanks);
     }
 
     /*
@@ -22,31 +22,48 @@ export class TaxonomicUnitService extends BaseService<TaxonomicUnit>{
         const { limit, offset, ...qParams } = params;
 
         return await (qParams.id)?
-            this.myRepository.find({
+            this.taxonRanks.find({
                 take: limit,  // Set default limit high since table is small
                 skip: offset,
                 where: { id: In(params.id) }
             })
-            : this.myRepository.find({take: limit, skip: offset})
+            : this.taxonRanks.find({take: limit, skip: offset})
     }
 
     /*
     TODO: Not sure if this is implemented correctly.
      */
     async create(data: Partial<TaxonomicUnit>): Promise<TaxonomicUnit> {
-        const taxon = this.myRepository.create(data);
-        return this.myRepository.save(taxon);
+        const taxon = this.taxonRanks.create(data);
+        return this.taxonRanks.save(taxon);
     }
 
     /*
     TODO: Implement
      */
     async updateByID(id: number, data: Partial<TaxonomicUnit>): Promise<TaxonomicUnit> {
-        const updateResult = await this.myRepository.update({ id }, data);
+        const updateResult = await this.taxonRanks.update({ id }, data);
         if (updateResult.affected > 0) {
             return this.findByID(id);
         }
         return null;
     }
 
+    async getAncestorRanks(rank: TaxonomicUnit): Promise<TaxonomicUnit[]> {
+        return this.taxonRanks.find({
+            kingdomName: rank.kingdomName,
+            rankID: LessThan(rank.rankID)
+        });
+    }
+
+    async getMostSpecificRank(ranks: TaxonomicUnit[]): Promise<TaxonomicUnit> {
+        return ranks.reduce((previous, current) => {
+            return previous.rankID > current.rankID ? previous : current;
+        }, ranks[0]);
+    }
+
+    async getDirectParentRank(rank: TaxonomicUnit): Promise<TaxonomicUnit> {
+        const allAncestors = await this.getAncestorRanks(rank);
+        return await this.getMostSpecificRank(allAncestors);
+    }
 }
