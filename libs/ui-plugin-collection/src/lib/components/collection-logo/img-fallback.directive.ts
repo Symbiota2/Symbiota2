a6ac68d3a51ed24b5aa8b61f649f0cfeded85a98
Darius <dariusdum@gmail.com>
2021-12-21 17:55:37 -0700

    changed assets string into a constant representation

--- libs/ui-plugin-collection/src/lib/components/collection-logo/img-fallback.directive.ts
+++ libs/ui-plugin-collection/src/lib/components/collection-logo/img-fallback.directive.ts
@@ -6,10 +6,12 @@ import { ElementRef } from '@angular/core';
 })
 export class ImgFallbackDirective {
 
+  readonly DEFAULT_ICON_PATH = "assets/images/default_av.png"
+
   constructor(private el: ElementRef) { }
 
   @HostListener('error') onError() {
-    this.el.nativeElement.src = 'assets/images/default_av.png'
+    this.el.nativeElement.src = this.DEFAULT_ICON_PATH
     
   }
 }
