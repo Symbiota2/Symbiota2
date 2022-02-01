6254172fc111089ce3205d9a760bbc3940a5b7d2
cdyreson@gmail.com <4VJ6ioNkY8aANn3i>
2022-01-31 21:54:10 -0700

    Another try at fixing demo site issue.

--- libs/ui-plugin-taxonomy/src/lib/dto/taxon-status-list-item.ts
+++ libs/ui-plugin-taxonomy/src/lib/dto/taxon-status-list-item.ts
@@ -1,6 +1,5 @@
 import { Exclude, Expose, plainToClass, Type } from 'class-transformer';
 import { TaxonListItem } from '@symbiota2/ui-plugin-taxonomy';
-import { ImageListItem } from '@symbiota2/ui-plugin-image';
 
 @Exclude()
 export class TaxonomicStatusListItem {
