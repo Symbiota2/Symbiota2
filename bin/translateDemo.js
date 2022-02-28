b6ed3572ef22c8a403bbfe89b5cc4023e617d007
DovahCraft <josephvargovich@gmail.com>
2022-02-27 21:59:23 -0700

    Script ready to copy and translate

--- bin/translateDemo.js
+++ bin/translateDemo.js
@@ -10,6 +10,44 @@ const symbiotaKey = fs.readFileSync("cloudKey.txt").toString('utf-8');
 console.log(symbiotaKey);
 
 
+/*function translateJsons(languages, languagePrefixes) {
+  //console.log("Languages dict", languages)
+  let enPaths = languages["en.json"]
+  var index = 0
+  enPaths.forEach(path => {
+    console.log("Translating using en path ", index, path);
+
+    index++;
+  });
+  //console.log(enPaths);
+}*/
+
+function translateJson(enContents, targetLangPath) {
+  //Parse the english Json
+  const contents = fs.readFileSync(targetLangPath).toString();
+  try {
+    const contentsJson = JSON.parse(contents);
+    console.log("DOING TRANSLATE IG")
+  }
+  catch (e) {
+    console.log("Translate json errored", e)
+    throw `Invalid JSON at ${targetLangPath}: ${e}`;
+  }
+  //console.log(enPaths);
+}
+
+async function translateText() {
+  // Translates the text into the target language. "text" can be a string for
+  // translating a single piece of text, or an array of strings for translating
+  // multiple texts.
+  let [translations] = await translate.translate(text, target);
+  //translations = Array.isArray(translations) ? translations : [translations];
+  console.log('Translations:');
+  translations.forEach((translation, i) => {
+    console.log(`${text[i]} => (${target}) ${translation}`);
+  });
+}
+
 
 //Make sure you have the key file needed to use the API stored locally! Find it on GCloud.
 //This allows us to use a generic API key instead of an application service account for authentication.
@@ -37,17 +75,19 @@ const pluginPattern = path.resolve(
 
 const languages = {};
 
-const languagePrefixes = { 'en': 'English', 'ru': 'Russian' }
+const languagePrefixes = ['en', 'ar', 'de', 'es', 'fa', 'fr', 'hi', 'it', 'ja', 'pt', 'ru', 'ur', 'zh'];
+console.log("Finding es: ", languagePrefixes.find(lang => lang == 'es'));
+
 
 
 const outDir = path.resolve(path.resolve(__dirname, "..", "apps", "ui", "src", "assets", "i18n"));
 const outDirApps = path.resolve(path.resolve(__dirname, "..", "apps"));
-console.log("OutDir: ", outDir)
+console.log("OutDir: ", outDir);
 
-console.log("Pluginpattern:", pluginPattern)
+console.log("Pluginpattern:", pluginPattern);
 
-const text = ['What\'s up guys welcome to Symbiota2', 'SOmething'];
-const target = 'es';
+//const text = ['What\'s up guys welcome to Symbiota2', 'SOmething'];
+//const target = 'es';
 
 [srcPattern, pluginPattern].forEach((pattern) => {
   glob.sync(pattern)
@@ -55,8 +95,8 @@ const target = 'es';
     .forEach((file) => {
       const baseName = path.basename(file);
       const langKeys = Object.keys(languages);
-      console.log("Path: ", path.dirname(file))
-      console.log("BaseName: ", baseName.toString())
+      //console.log("Path: ", path.dirname(file))
+      //console.log("BaseName: ", baseName.toString())
       if (!langKeys.includes(baseName)) {
         languages[baseName] = [];
       }
@@ -64,18 +104,42 @@ const target = 'es';
     });
 });
 
-console.log("Languages dict", languages)
+//translateJsons(languages, languagePrefixes)
+//console.log("Languages dict", languages)
 
-async function translateText() {
-  // Translates the text into the target language. "text" can be a string for
-  // translating a single piece of text, or an array of strings for translating
-  // multiple texts.
-  let [translations] = await translate.translate(text, target);
-  //translations = Array.isArray(translations) ? translations : [translations];
-  console.log('Translations:');
-  translations.forEach((translation, i) => {
-    console.log(`${text[i]} => (${target}) ${translation}`);
-  });
-}
+let enPaths = languages["en.json"]
+var index = 0
+enPaths.forEach(path => {
+  console.log("Translating using en path ", index, path);
+  var langIndex = 1;
+  let currEnPath = enPaths[index];
+  console.log("Attempting to read English json at path: ", currEnPath)
+  //Parse the english Json
+  const contents = fs.readFileSync(currEnPath).toString();
+  console.log("Contents: ", contents)
+  try {
+    const enContentsJson = JSON.parse(contents);
+    console.log("Did we get here?")
+    //If we can read the english json at the directory, commence translations.
+    Object.keys(languages).forEach(langKey => {
+      if (langKey != "en.json") {
+        let targetLangPath = languages[langKey][index];
+        console.log("LangKey: ", langKey);
+        console.log("Lang Json Paths", langIndex, languages[langKey][0]);
+        translateJson(enContentsJson, targetLangPath)
+      }
+    })
+  }
+  catch (e) {
+    //console.log("Could not read json at path: ", currEnPath, " continuing to next directory.");
+    //console.log(`Error: ${e}`);
+    //throw `Invalid JSON at ${file}: ${e}`;
+  }
+
+
+  index++;
+});
+  //console.log(enPaths);
+
 
 //translateText();
