9e6aff0460c581d7b01e56dea2573ec62ec0a1e8
DovahCraft <josephvargovich@gmail.com>
2022-02-28 17:53:49 -0700

    Removed extra en file path in ui-common

--- bin/perform-translations.js
+++ bin/perform-translations.js
@@ -9,18 +9,26 @@ const glob = require("glob");
 const symbiotaKey = fs.readFileSync("cloudKey.txt").toString('utf-8');
 
 
-function translateJson(enContents, targetLangPath) {
+function translateJson(enContents, languages, jsonFileIndex) {
   //Parse the english Json
-  const contents = fs.readFileSync(targetLangPath).toString();
+  const targetLangPrefixes = ['ar', 'de', 'es', 'fa', 'fr', 'hi', 'it', 'ja', 'pt', 'ru', 'ur', 'zh'];
+  //console.log("About to read languages: ", languages);
+  const contents = fs.readFileSync(languages['es.json'][jsonFileIndex]).toString();
+  const englishKeys = Object.keys(enContents);
+  targetLangPrefixes.forEach(langPrefix => {
+    let langKey = langPrefix + ".json";
+    console.log("Lang Prefix curr: ", langPrefix);
+    console.log("LangKey curr: ", langKey);
+    console.log("Lang Prefix currPath: ", languages[langKey][jsonFileIndex]);
+  })
   try {
     const contentsJson = JSON.parse(contents);
     console.log("DOING TRANSLATE")
   }
   catch (e) {
     console.log("Translate json errored", e)
-    throw `Invalid JSON at ${targetLangPath}: ${e}`;
+    throw `Error in translateJson(): ${e}`;
   }
-  console.log(Object.keys(enContents));
 }
 
 async function translateText() {
@@ -61,14 +69,9 @@ const pluginPattern = path.resolve(
 );
 
 const languages = {};
-
-const languagePrefixes = ['en', 'ar', 'de', 'es', 'fa', 'fr', 'hi', 'it', 'ja', 'pt', 'ru', 'ur', 'zh'];
-console.log("Finding es: ", languagePrefixes.find(lang => lang == 'es'));
-
-
-
-const outDir = path.resolve(path.resolve(__dirname, "..", "apps", "ui", "src", "assets", "i18n"));
-const outDirApps = path.resolve(path.resolve(__dirname, "..", "apps"));
+const outDir = path.resolve(__dirname, "..", "apps", "ui", "src", "assets", "i18n");
+const extraEnDir = path.resolve(__dirname, "..", "libs", "ui-common")
+const outDirApps = path.resolve(__dirname, "..", "apps");
 console.log("OutDir: ", outDir);
 
 console.log("Pluginpattern:", pluginPattern);
@@ -79,6 +82,7 @@ console.log("Pluginpattern:", pluginPattern);
 [srcPattern, pluginPattern].forEach((pattern) => {
   glob.sync(pattern)
     .filter((file) => !file.startsWith(outDirApps))
+    .filter((file) => !file.startsWith(extraEnDir))
     .forEach((file) => {
       const baseName = path.basename(file);
       const langKeys = Object.keys(languages);
@@ -92,34 +96,36 @@ console.log("Pluginpattern:", pluginPattern);
 });
 
 //translateJsons(languages, languagePrefixes)
-//console.log("Languages dict", languages)
+console.log("Languages dict", languages)
 
 let enPaths = languages["en.json"]
 var index = 0
+//Compare each english path (file) to each other language file and translate
 enPaths.forEach(path => {
   console.log("Translating using en path ", index, path);
-  var langIndex = 1;
   let currEnPath = enPaths[index];
-  //console.log("Attempting to read English json at path: ", currEnPath)
+  console.log("Attempting to read English json at path: ", currEnPath)
   //Parse the english Json
   const contents = fs.readFileSync(currEnPath).toString();
   //console.log("Contents: ", contents)
   try {
-    const enContentsJson = JSON.parse(contents);
+    const enContentsObj = JSON.parse(contents);
+    //console.log("Calling translateJson with languages: ", languages);
+    translateJson(enContentsObj, languages, index)
     //If we can read the english json at the directory, commence translations.
-    Object.keys(languages).forEach(langKey => {
+    /*Object.keys(languages).forEach(langKey => {
       if (langKey != "en.json") {
-        let targetLangPath = languages[langKey][index];
+        //let languages = languages[langKey];
         //    console.log("LangKey: ", langKey);
         //    console.log("Lang Json Paths", langIndex, languages[langKey][0]);
-        translateJson(enContentsJson, targetLangPath)
+        
       }
-    })
+    })*/
   }
   catch (e) {
     //console.log("Could not read json at path: ", currEnPath, " continuing to next directory.");
     //console.log(`Error: ${e}`);
-    //throw `Invalid JSON at ${file}: ${e}`;
+    throw `Invalid JSON in main foreach: ${e}`;
   }
 
 
