f99306750248d1cfdc8ab76fc729062389301abe
Darius <dariusdum@gmail.com>
2022-03-02 18:00:01 -0700

    added banner, added tpt assets and text to homepage

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -2,6 +2,9 @@ export const environment = {
     production: true,
     apiUrl: "BACKEND URL GOES HERE",
     defaultLanguage: "en",
-    appTitle: "My Symbiota2 Portal",
-    recaptchaSiteKey: ""
+    appTitle: "Terrestrial Parasite Tracker",
+    recaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+    // For mapping functionality
+    tilesUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    tilesToken: ""
 };
