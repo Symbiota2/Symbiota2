a95764c46adbe2a14b1ea0ca4a10cc3950bf5438
Evin Dunn <evindunn14@gmail.com>
2022-04-13 06:03:10 -0700

    Adds comments for dwc archive parser

--- libs/dwc/src/lib/archive/parser.ts
+++ libs/dwc/src/lib/archive/parser.ts
@@ -10,12 +10,25 @@ import csv from 'csv-parser';
 import { createReadStream, mkdtempSync } from 'fs';
 import { rm } from 'fs/promises';
 
+/**
+ * Parse a DwC Archive file & return Taxon entities
+ * Archives are zip files composed of a meta.xml with metadata & csv files with actual data
+ * See https://dwc.tdwg.org/text/ for more
+ */
 export class DwCArchiveParser {
+
+    /**
+     * @param filepath The path to the dwc archive
+     * @param dwcType The type of dwc class to parse the archive for. 
+     * See "rowType" in https://dwc.tdwg.org/text/. To transform a dwc class into a database
+     * entity, use a static member DWC_TYPE on the entity class.
+     */
     static async *read(filepath: string, dwcType: string) {
-        // TODO: App config data dir?
+        // TODO: App config temp dir?
         const tempdir = mkdtempSync(`${tmpdir()}${path.sep}`);
         const meta = await DwCArchiveParser.parseMeta(filepath, tempdir);
 
+        // Target stores the "core" section of meta.xml that corresponds to the dwcType
         let target = null;
         if (meta.archive.core.$.rowType === dwcType) {
             target = meta.archive.core;
@@ -33,7 +46,11 @@ export class DwCArchiveParser {
             throw new Error(`'${dwcType}' not found in ${filepath}!`);
         }
 
+        // Get an ordered array of fields from meta.xml
         const fields = DwCArchiveParser.parseFields(target.field);
+
+        // Extract the location (path) of data files for dwcType from meta.xml
+        // See the <files> element in https://dwc.tdwg.org/text/
         const coreFiles = target.files.reduce((fileList, currentFiles) => {
             return [
                 ...fileList,
@@ -41,6 +58,7 @@ export class DwCArchiveParser {
             ];
         }, []);
 
+        // Use csv-parser to read each row of each data csv and yield it as an iterator
         for (const file of coreFiles) {
             const stream = createReadStream(file).pipe(
                 csv({
@@ -55,14 +73,25 @@ export class DwCArchiveParser {
             }
         }
 
+        // Cleanup
         await rm(tempdir, { recursive: true });
     }
 
+    /**
+     * Sort the fields in an array of IDwCAMetaFieldTypes by their index attribute.
+     * See the "<field>" element in https://dwc.tdwg.org/text/
+     * @param fields A list of <field> elements read from a dwc meta.xml
+     */
     private static parseFields(fields: IDwCAMetaFieldType[]) {
         return fields.sort((a, b) => a.$.index - b.$.index).map((f) => f.$.term);
     }
 
-    private static async parseMeta(archiveFilePath: string, tempdir: string) {
+    /**
+     * @param archiveFilePath The path to the darwin core archive
+     * @param tempdir A temporary directory to extract meta.xml to
+     * @returns An object containing the archive metadata
+     */
+    private static async parseMeta(archiveFilePath: string, tempdir: string): Promise<IDwCAMeta> {
 
         await zipExtract(archiveFilePath, { dir: tempdir });
         const archiveMeta = await readXmlFile<IDwCAMeta>(
