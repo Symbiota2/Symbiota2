c9c986a307e995daa6e650db9464d4cbe62362f9
Joseph Vargovich <josephvargovich@gmail.com>
2022-05-10 19:27:28 +0000

    Super admin userlist and permission panels created

--- apps/ui/src/app/components/userlist/userlist.component.ts
+++ apps/ui/src/app/components/userlist/userlist.component.ts
@@ -1,18 +1,40 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserOutputDto } from '@symbiota2/api-auth';
+import { UserPanel } from '../../pages/userlist-page/userpanel-data';
 
 @Component({
   selector: 'symbiota2-userlist',
   templateUrl: './userlist.component.html',
   styleUrls: ['./userlist.component.scss']
 })
+
 export class UserlistComponent implements OnInit {
+
+
   @Input() userList: UserOutputDto[];
-  panelOpenState = false;
+  @Input() userPanelList: UserPanel[];
+  panelOpenState = true;
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /*hasPermission(userPanel: UserPanel, targetPermission: String): boolean {
+    userPanel.permissions.forEach(role => {
+      console.log("Checking targetPermission: ", typeof targetPermission, " against permission: ", typeof role.name);
+      if (role.name === targetPermission) {
+        console.log("Returning true");
+        return true;
+      }
+
+    })
+    return false;
+  } */
+
 }
+
